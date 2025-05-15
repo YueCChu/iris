0f6ed2aab64500b9eba2be25708793fff113461b
YueCChu <102800969+YueCChu@users.noreply.github.com>
2025-05-15 21:43:46 +0800

    Update deepseek.py

--- src/models/deepseek.py
+++ src/models/deepseek.py
@@ -13,7 +13,7 @@ class DeepSeekModel(LLM):
     def __init__(self, model_name, logger: MyLogger, **kwargs):
         # 这里传入 _api_model_name_map
         super().__init__(model_name, logger, _api_model_name_map, **kwargs)
-        self.api_key = "34237fbd-8bfd-4efb-9dd8-18623c4cacfb"
+        self.api_key = "xxxx"
         self.base_url = "https://ark.cn-beijing.volces.com/api/v3"
         self.model = _api_model_name_map[model_name]  # 直接传入推理接入点ID
         self.client = OpenAI(base_url=self.base_url, api_key=self.api_key)
